36b717fe95676bf2786d6f833b047ef7c1f969e9
Noah Chin-Shue <148300785+csnoa@users.noreply.github.com>
2024-05-12 12:30:26 -0400

    Update thing.js

--- thing.js
+++ thing.js
@@ -18,6 +18,15 @@ window.onload = (event) => {
     }
 };
 
+document.addEventListener("change", function() {
+    const list = element.querySelectorAll("*");
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].style.zindex == 2147483647)
+            this.alert("Found1");
+    }
+}); 
+
 document.addEventListener("DOMContentLoaded", (event) => {
   const list = element.querySelectorAll("*");
 
