82d92abff69de7a02ffafcf57eefd069f0da11fe
Noah Chin-Shue <148300785+csnoa@users.noreply.github.com>
2024-05-12 12:23:06 -0400

    Update thing.js

--- thing.js
+++ thing.js
@@ -8,3 +8,21 @@ window.addEventListener('load', function () {
             alert("It's loaded!");
     }
 })
+
+window.onload = (event) => {
+    const list = element.querySelectorAll("*");
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].style["z-index"] == 2147483647)
+            alert("It's loaded!");
+    }
+};
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const list = element.querySelectorAll("*");
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].style["z-index"] == 2147483647)
+            alert("It's loaded!");
+    }
+});
